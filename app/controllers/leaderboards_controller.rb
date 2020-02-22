4b94654561ae798f872b600f8b629ef9828c0a27
illnull <lolshinepro@gmail.com>
2020-02-22 01:37:31 -0600

    Added leaderboard controller

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class LeaderboardsController < ApplicationController
   def index
+    @leaderboards = Leaderboard.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'leaderboards/index'
-  get 'masteries/index'
-  get 'masteries/show'
   resources :pages
   root to: 'about_us#index'
 
   resources 'players', only: %i[index show]
 
   resources 'summoners', only: %i[index show]
+
+  resources 'leaderboards', only: %i[index]
 end
