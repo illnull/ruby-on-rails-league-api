0b0ed3a61fd26934500fc60b5b3c6fda196584b6
illnull <lolshinepro@gmail.com>
2020-02-21 23:44:53 -0600

    FIXED THE ASSICIATION WITH SUMMONER AND LEADERBOARD (TYPOS)

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Leaderboard < ApplicationRecord
-  has_many :summoners_leaderboards
+  has_many :summoners_leaderboads
+  has_many :summoners, through: :summoners_leaderboads
 
   # validates :queueType, :wins, :losses, :leaguePoints, presence: true
 end
--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -3,6 +3,8 @@
 class Summoner < ApplicationRecord
   belongs_to :player
   has_many :masteries
+  has_many :leaderboards
+  has_many :leaderboards, through: :summoners_leaderboads
 
   validates :summonerId, :accountId, :summonerName, :summonerLevel, presence: true
 end
