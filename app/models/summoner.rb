d10853b523a39aa9f457bdb17ab697a63d3533f3
illnull <lolshinepro@gmail.com>
2020-02-21 23:47:33 -0600

    THE REAL FIX(MORE TYPOS)

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -3,7 +3,7 @@
 class Summoner < ApplicationRecord
   belongs_to :player
   has_many :masteries
-  has_many :leaderboards
+  has_many :summoners_leaderboads
   has_many :leaderboards, through: :summoners_leaderboads
 
   validates :summonerId, :accountId, :summonerName, :summonerLevel, presence: true
